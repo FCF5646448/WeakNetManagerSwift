62029fee5227f023d0912d961cbc525459e8b189
FCF5646448 <you@example.com>
2018-06-13 15:42:47 +0800

    Initial Commit

---
+++ WeakNetManagerSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeakNetManagerSwift
+//
+//  Created by 冯才凡 on 2018/6/13.
+//  Copyright © 2018年 冯才凡. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
